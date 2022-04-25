Fixed scaling for estro significant model. Had hardcoded a scaling 1, 2, 2, but should have been dynamic to input.

model_repository/models/1_22-03-3_oar_bounds_estro_significant_oars/img/ops/op_down_scale.py
@@ -1,6 +1,6 @@
 import logging
 from multiprocessing.pool import ThreadPool
-from typing import Dict
+from typing import Dict, Tuple
 
 import monai.deploy.core as md
 import numpy as np
@@ -12,6 +12,7 @@ from .timer import TimeOP
 @md.input("label_array_dict", Dict[str, np.ndarray], IOType.IN_MEMORY)
 @md.input("ref_image", sitk.Image, IOType.IN_MEMORY)
 @md.output("label_array_dict", Dict[str, np.ndarray], IOType.IN_MEMORY)
+@md.output("scale_factor", Tuple, IOType.IN_MEMORY)
 @md.env(pip_packages=["monai==0.6.0", "simpleitk", "numpy", "scikit-image"])
 class DownScaleImages(Operator):
     def __init__(self):
@@ -26,22 +27,18 @@ class DownScaleImages(Operator):
         ref_arr = sitk.GetArrayFromImage(ref_img)
         ref_dim = ref_arr.shape
 
-        t = ThreadPool(4)
-        results = t.starmap(self.down_scale, self.arg_yield(arrs, ref_dim))
-        t.close()
-        t.join()
-        op_output.set({t[0]: t[1] for t in results}, label="label_array_dict")
+        scale_factor = self.get_scale_factor(arrs["tmp_0001.nii.gz"], ref_dim)
+        op_output.set(scale_factor, "scale_factor")
+        arrs["tmp_0001.nii.gz"] = self.down_scale(arrs["tmp_0001.nii.gz"], scale_factor)
+        op_output.set(arrs, label="label_array_dict")
+
         print(timer.report())
 
-    def arg_yield(self, arrs, ref_dim):
-        for label, arr in arrs.items():
-            yield label, arr, ref_dim
+    def get_scale_factor(self, img_arr, ref_dim):
+        scale_factor = tuple(np.array(img_arr.shape) // np.array(ref_dim))
+        logging.info(scale_factor)
+        return scale_factor
 
-    def down_scale(self, label, img_arr, ref_dim):
-        scale_factor = np.array(img_arr.shape) // np.array(ref_dim)
-        logging.info(f"Scale factor: {scale_factor}")
-
-        reduced_arr = skimage.measure.block_reduce(img_arr, tuple(scale_factor), np.max)
-
-        logging.info(f"Reduced image has dimentions arr:{reduced_arr.shape}")
-        return label, reduced_arr
+    def down_scale(self, img_arr, scale_factor: Tuple):
+        reduced_arr = skimage.measure.block_reduce(img_arr, scale_factor, np.max)
+        return reduced_arr